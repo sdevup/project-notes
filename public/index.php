58843d39d3f265138056db576fc01572d3e729e4
Silva <silvadevbr.recovery@gmail.com>
2026-03-28 23:38:47 -0300

    28/03

--- public/index.php
+++ public/index.php
@@ -2,6 +2,28 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Servir arquivos estáticos
+$filePath = __DIR__ . $_SERVER['REQUEST_URI'];
+if (is_file($filePath) && file_exists($filePath)) {
+    // Definir o tipo de conteúdo apropriado
+    $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'ico' => 'image/x-icon',
+    ];
+    
+    if (isset($mimeTypes[$ext])) {
+        header('Content-Type: ' . $mimeTypes[$ext]);
+    }
+    
+    return readfile($filePath);
+}
+
 // Configurar autoload para App
 spl_autoload_register(function ($class) {
     if (strpos($class, 'App\\') === 0) {
@@ -16,6 +38,8 @@ $router = require __DIR__ . '/../routes/web.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'] ?? '/';
+
+$response = $router->dispatch($method, $path);
 
 $response = $router->dispatch($method, $path);
 
