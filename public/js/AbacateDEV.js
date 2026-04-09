b067a9046b85b71791e849b75a56706923194065
Pedro Emanuel <penuelpaifurta@gmail.com>
2026-04-09 15:00:33 -0300

    Atualização
    Atualização

---
+++ public/js/AbacateDEV.js
@@ -0,0 +1,11 @@
+var mensagem = 'Olá, mundo!';
+
+
+
+
+
+document.querySelectorAll('.teste2').forEach(element => {
+    element.addEventListener('click', () => {
+        alert('Teste 2');
+    });
+});
--- resources/views/test.render.php
+++ resources/views/test.render.php
@@ -7,16 +7,38 @@
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    
-    <!-- Adiciona o header contido em /partials-->
+
     @include('partials.header')
 
     <h1>Página Teste</h1>
- 
-    <!-- Adiciona o footer contido em partials-->
+
+    <div class="card">
+        <img src="https://image.api.playstation.com/vulcan/img/cfn/11307fllh6D-IvbpCa18N0vRggVeRYWA06paTNCj3DENJMScAzW2f3ry4IwFcXBAt9kWXdZGpGoOGjxJ_e9MdordMVAosNhZ.png" alt="Foto">
+        <p class = "ps5"> AbacateDEV - Platina CUPHEAD</p>
+        
+         <button class="teste2">Botão</button>
+         
+    </div>
+
+      <div class="card">
+        <img src="https://image.api.playstation.com/vulcan/img/cfn/11307fllh6D-IvbpCa18N0vRggVeRYWA06paTNCj3DENJMScAzW2f3ry4IwFcXBAt9kWXdZGpGoOGjxJ_e9MdordMVAosNhZ.png" alt="Foto">
+        <p>AbacateDEV - Platina CUPHEAD</p>
+        
+         <button class="teste2">Botão</button>
+         
+    </div>
+
+     <div class="card">
+        <img src="https://image.api.playstation.com/vulcan/img/cfn/11307fllh6D-IvbpCa18N0vRggVeRYWA06paTNCj3DENJMScAzW2f3ry4IwFcXBAt9kWXdZGpGoOGjxJ_e9MdordMVAosNhZ.png" alt="Foto">
+        <p>AbacateDEV - Platina CUPHEAD</p>
+        
+         <button class="teste2">Botão</button>
+
+    </div>
+
     @include('partials.footer')
 
-     
     <script src="/js/script.js"></script>
+    <script src="/js/AbacateDEV.js"></script>
 </body>
 </html>
