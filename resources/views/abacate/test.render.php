3928356ead44598eeb57de93213381d5279da8ae
Pedro Emanuel <penuelpaifurta@gmail.com>
2026-04-21 11:24:48 -0300

    Alterações CSS
    CSS

--- resources/views/abacate/test.render.php
+++ resources/views/abacate/test.render.php
@@ -10,33 +10,51 @@
 
     @include('components.header')
 
-    <h1>Teste Abacate</h1>
+    <h1>Cards Test</h1>
 
     <main class="fundo my-5">
         <div class="container-abacate">
             
             <div class="card-abacate">
                 <img src="https://image.api.playstation.com/vulcan/img/cfn/11307fllh6D-IvbpCa18N0vRggVeRYWA06paTNCj3DENJMScAzW2f3ry4IwFcXBAt9kWXdZGpGoOGjxJ_e9MdordMVAosNhZ.png" 
-                     alt="Cuphead" style="width: 100px;">
+                     alt="Cuphead">
                 <h5 class="ps5">Cuphead</h5>
                 <button class="btn teste2">Clique aqui</button>
             </div>
 
             <div class="card-abacate">
                 <img src="https://upload.wikimedia.org/wikipedia/en/d/de/Hollow_Knight_2026_cover_art.jpg" 
-                     alt="Hollow Knight" style="width: 100px;">
+                     alt="Hollow Knight">
                 <h5 class="ps5">Hollow Knight</h5>
                 <button class="btn teste2">Clique aqui</button>
             </div>
 
             <div class="card-abacate">
-                <img src="https://assetsio.gnwcdn.com/ori-and-the-blind-forest-developer-accused-of-bullying-staff-1647862013180.jpg?width=570&quality=85&format=jpg&dpr=3&auto=webp" 
-                     alt="Ori" style="width: 180px;">
+                <img src="https://assetsio.gnwcdn.com/ori-and-the-blind-forest-developer-accused-of-bullying-staff-1647862013180.jpg" 
+                     alt="Ori">
                 <h5 class="ps5">Ori and the Blind Forest</h5>
                 <button class="btn teste2">Clique aqui</button>
             </div>
 
         </div>
     </main>
+
+    <script>
+document.addEventListener('DOMContentLoaded', () => {
+
+    const botao = document.querySelector('.botao_menu');
+    const menu = document.querySelector('.menu-nav');
+
+    console.log('botao:', botao);
+    console.log('menu:', menu);
+
+    botao.addEventListener('click', () => {
+        console.log('clicou!');
+        menu.classList.toggle('ativo');
+    });
+
+});
+</script>
+
 </body>
 </html>
--- resources/views/components/header.render.php
+++ resources/views/components/header.render.php
@@ -1,5 +1,5 @@
 <header class="bg-dark text-light p-3 mb-4">
-  <div class="container-xl d-flex justify-content-between align-items-center flex-wrap">
+  <div class="container-xl d-flex justify-content-between align-items-center flex-wrap position-relative">
 
     <div class="logo">
       <a class="text-light" href="/" style="font-weight: 700;">
@@ -7,15 +7,17 @@
       </a>
     </div>
 
-    <nav class="d-flex flex-wrap justify-content-center">
+    <!-- MENU -->
+    <nav class="menu-nav d-flex flex-wrap justify-content-center">
       <a class="navegacao m-2 text-light" href="/">Home</a>
-      <a class=" navegacao m-2 text-light" href="/login">Login</a>
-      <a class=" navegacao m-2 text-light" href="/principal">Principal</a>
+      <a class="navegacao m-2 text-light" href="/login">Login</a>
+      <a class="navegacao m-2 text-light" href="/principal">Principal</a>
       <a class="navegacao m-2 text-light" href="/test">Pedro DEV</a>
       <a class="navegacao m-2 text-light" href="/test2">Sdev</a>
     </nav>
 
-    <svg class="botao_menu" border="2" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+    <!-- BOTÃO (☰) -->
+    <svg class="botao_menu bi bi-list" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16" style="cursor: pointer;">
       <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
     </svg>
 
